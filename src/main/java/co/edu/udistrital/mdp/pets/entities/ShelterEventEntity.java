48ab3d1bc1cc9eaf186adad4f47b78ef8fb3b057
Xiomara ! <xiomarafuentes1811@gmail.com>
2026-03-08 21:57:02 -0500

    feat: add asociations

--- src/main/java/co/edu/udistrital/mdp/pets/entities/ShelterEventEntity.java
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/ShelterEventEntity.java
@@ -1,7 +1,10 @@
 package co.edu.udistrital.mdp.pets.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
+
 import java.time.LocalDate;
 
 @Data
@@ -12,5 +15,10 @@ public class ShelterEventEntity extends BaseEntity {
     LocalDate fecha;
     String name;
     String description;
+
+    //Asociation with Shelter Entity, one shelter has many events.
+    @PodamExclude
+    @ManyToOne
+    private ShelterEntity shelter;
 
 }
