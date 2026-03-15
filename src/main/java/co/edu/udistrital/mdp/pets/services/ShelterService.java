97d1cd4a1f05ee79e24e79fd50f8f8ce65043263
Xiomara ! <xiomarafuentes1811@gmail.com>
2026-03-15 13:23:13 -0500

    feat: update shelter, validate shelter, fix: problems with create shelter

--- src/main/java/co/edu/udistrital/mdp/pets/services/ShelterService.java
+++ src/main/java/co/edu/udistrital/mdp/pets/services/ShelterService.java
@@ -10,23 +10,21 @@ import co.edu.udistrital.mdp.pets.repositories.ShelterRepository;
 
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class ShelterService {
-
     @Autowired
-    ShelterRepository shelterRepository;
+    //Repositorio de refugios, para persistencia.
+    private ShelterRepository shelterRepository;
 
     //Definir regex para validar correo.
     String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
 
-    //Metodo para crear un refugio.
-    @Transactional
-    public ShelterEntity createShelter(ShelterEntity shelterEntity) throws EntityNotFoundException, IllegalOperationException {
-    
-        log.info("Start shelter creation...");
-
+    //Metodo privado para evaluar si son válidos o no los datos del refugio.
+    private void validateShelter(ShelterEntity shelterEntity)
+    throws IllegalOperationException {
         //Validar si el nombre es nulo.
         if(shelterEntity.getName() == null)
             throw new IllegalOperationException("Name isn't valid.");
@@ -43,8 +41,21 @@ public class ShelterService {
         if(shelterEntity.getEmail() == null)
             throw new IllegalOperationException("Email isn't valid.");
 
-        //Validar si solo hay un refugio con ese nombre.
+        //Validar si el formato del correo es correcto.
+        if(!shelterEntity.getEmail().matches(regex))
+            throw new IllegalOperationException("Email format isn't valid.");
+    }
 
+    //Metodo para crear un refugio.
+    @Transactional
+    public ShelterEntity createShelter(ShelterEntity shelterEntity) 
+    throws EntityNotFoundException, IllegalOperationException {
+    
+        log.info("Start shelter creation...");
+        //Usar el método privado para evitar repetir código.
+        validateShelter(shelterEntity);
+
+        //Validar si solo hay un refugio con ese nombre.
         if(shelterRepository.findByName(shelterEntity.getName()).isPresent())
             throw new IllegalOperationException("There is already a shelter with that name.");
 
@@ -52,11 +63,47 @@ public class ShelterService {
         if(shelterRepository.findByEmail(shelterEntity.getEmail()).isPresent())
             throw new IllegalOperationException("There is already a shelter with that email.");
 
-        //Validar si el formato del correo es correcto.
-        if(!shelterEntity.getEmail().matches(regex))
-            throw new IllegalOperationException("Email format isn't valid.");
-
         //Si cumple con todo se permitirá crear un refugio.
         return shelterRepository.save(shelterEntity);
+    }
+
+    //Metodo para actualizar un refugio.
+    @Transactional
+    public ShelterEntity updateShelter(long shelterId, ShelterEntity shelter) 
+     throws EntityNotFoundException, IllegalOperationException {
+        log.info("Start update shelter with id: {}", shelterId);
+        //Busca el refugio.
+        Optional<ShelterEntity> shelterEntity = shelterRepository.findById(shelterId);
+
+        //Verifica su existencia.
+        if(shelterEntity.isEmpty())
+            throw new EntityNotFoundException("Shelter not found.");
+
+        //Obtiene el refugio a actualizar.
+        ShelterEntity shelterToUpdate = shelterEntity.get();
+        
+        //Validacion de datos.
+        validateShelter(shelter);
+
+        //Validar unicidad de nombre.
+        Optional<ShelterEntity> nameCheck = shelterRepository.findByName(shelter.getName());
+        if (nameCheck.isPresent() && !nameCheck.get().getId().equals(shelterId)) {
+            throw new IllegalOperationException("There is already a shelter with that name.");
+        }
+
+        //Validar unicidad de email.
+        Optional<ShelterEntity> emailCheck = shelterRepository.findByEmail(shelter.getEmail());
+        if (emailCheck.isPresent() && !emailCheck.get().getId().equals(shelterId)) {
+            throw new IllegalOperationException("There is already a shelter with that email.");
+        }
+
+        //Actualizar si cumple con todas las condiciones.
+        shelterToUpdate.setName(shelter.getName());
+        shelterToUpdate.setCity(shelter.getCity());
+        shelterToUpdate.setAddress(shelter.getAddress());
+        shelterToUpdate.setEmail(shelter.getEmail());
+
+        log.info("End update shelter with the id: {}", shelterId);
+        return shelterRepository.save(shelterToUpdate);
     }
 }
