6a44b54082c542adc0b6f0a7a35f19281633e085
santiagorsrd-ux <santiagorsrd@gmail.com>
2026-03-22 01:49:48 -0500

    feat: se añadio client, adoption and notification entities and repositories

---
+++ src/main/java/co/edu/udistrital/mdp/pets/entities/ClientEntity.java
@@ -0,0 +1,23 @@
+package co.edu.udistrital.mdp.pets.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.OneToMany;
+import uk.co.jemos.podam.common.PodamExclude;
+
+@Data
+@Entity
+
+public class ClientEntity extends BaseEntity {
+    
+    private String clientName ;
+    private String clientPhone;
+    private String clientEmail;
+
+    @PodamExclude
+    @OneToMany(mappedBy = "client")
+    private List<AdoptionEntity> adoptions = new ArrayList<>();
+
+}
