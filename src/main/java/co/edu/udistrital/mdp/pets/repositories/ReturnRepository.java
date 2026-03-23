cd6b4cf1adc284c093d27aa19c747cb821bfc11c
JulianDCamposA <jdcamposa@udistrital.edu.co>
2026-03-22 20:36:27 -0500

    fix: quite findByAdoptionId de  ReturnRepository y cambie de lugar los test a la capeta correspondeinte

--- src/main/java/co/edu/udistrital/mdp/pets/repositories/ReturnRepository.java
+++ src/main/java/co/edu/udistrital/mdp/pets/repositories/ReturnRepository.java
@@ -4,11 +4,7 @@ import co.edu.udistrital.mdp.pets.entities.ReturnEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ReturnRepository extends JpaRepository<ReturnEntity, Long> {
 
-    // Buscar la devolución asociada a una adopción específica
-    Optional<ReturnEntity> findByAdoptionId(Long adoptionId);
 }
