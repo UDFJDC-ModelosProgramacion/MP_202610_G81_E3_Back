cc4231883944f088d1ddd9f171acda1740481963
Xiomara ! <xiomarafuentes1811@gmail.com>
2026-03-22 22:00:52 -0500

    docs: add comments

--- src/main/java/co/edu/udistrital/mdp/pets/repositories/ShelterEventRepository.java
+++ src/main/java/co/edu/udistrital/mdp/pets/repositories/ShelterEventRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import co.edu.udistrital.mdp.pets.entities.ShelterEventEntity;
 import org.springframework.stereotype.Repository;
 
+//Repositorio para ShelterEventEntity, con métodos de búsqueda personalizados.
+
 @Repository
 public interface ShelterEventRepository extends JpaRepository <ShelterEventEntity, Long>{
     //Busqueda por nombre.
